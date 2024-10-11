8c2177107a28b2e05a69799aa0f41fc199d8d081
Ryan Pham <ryan_pham3671@hotmail.com>
2024-10-11 11:28:32 -0400

    remove vue2 version

--- movie-search-vue2/src/routes.js
+++
@@ -1,7 +0,0 @@
-import HomePage from './components/HomePage'
-import MovieDetails from './components/MovieDetails'
-
-export default [
-    { path: "/", component: HomePage },
-    { path: "/:id", component: MovieDetails }
-]
